9a5c193b64fec890e9837cd8806e02d6339813ee
bassem riachy <bassem.riachy@gmail.com>
2022-02-19 14:45:39 +0100

    Changed category to activity in trainings table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_19_130949) do
+ActiveRecord::Schema.define(version: 2022_02_19_134443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,7 +79,7 @@ ActiveRecord::Schema.define(version: 2022_02_19_130949) do
     t.bigint "user_id", null: false
     t.text "description"
     t.string "title"
-    t.string "category"
+    t.string "activity"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_trainings_on_user_id"
