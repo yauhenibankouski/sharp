c05ba07ef5a4a6738f858df73f002edc4e9058a1
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-02-19 19:02:07 +0100

    removed unnecessary spacing

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,8 +3,7 @@ class Booking < ApplicationRecord
   has_many :shared_training_plans
   belongs_to :client, class_name: 'User', foreign_key: "client_id"
 
-  validates :user,presence: true, uniqueness: { scope: :client }
+  validates :user, presence: true, uniqueness: { scope: :client }
   validates :client, presence: true
   validates :status, presence: true
-
 end
