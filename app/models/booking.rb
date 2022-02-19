66d7cbdd586482f32ae0a3e95d5fab33c6f5bcca
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-02-19 16:46:35 +0100

    added validations for booking, shared_exercise and shared_training_plan

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user
   has_many :shared_training_plans
+
+  validates :user, presence: true
+  validates :client_id, presence: true, uniqueness: { scope: :user } # Unique client-trainer pair
+  validates :status, presence: true
 end
