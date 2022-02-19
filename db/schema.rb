824c5f39b3604343618b18c1d9595f30fc1924b2
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-02-19 12:10:33 +0100

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,17 +15,6 @@ ActiveRecord::Schema.define(version: 2022_02_19_105814) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-
-  create_table "shared_exercises", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.integer "sets"
-    t.integer "repetitions"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_shared_exercises_on_user_id"
-
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -52,7 +41,17 @@ ActiveRecord::Schema.define(version: 2022_02_19_105814) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
+  create_table "shared_exercises", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "sets"
+    t.integer "repetitions"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_shared_exercises_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -71,8 +70,7 @@ ActiveRecord::Schema.define(version: 2022_02_19_105814) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
-  add_foreign_key "shared_exercises", "users"
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "shared_exercises", "users"
 end
