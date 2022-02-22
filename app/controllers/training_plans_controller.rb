ce07b2932e411ef55569a02a66d41a595d2cd177
bassem riachy <bassem.riachy@gmail.com>
2022-02-22 21:49:42 +0100

    New and create methods for the training plans controller

--- app/controllers/training_plans_controller.rb
+++ app/controllers/training_plans_controller.rb
@@ -1,9 +1,24 @@
 class TrainingPlansController < ApplicationController
 
-    def new
-    end
+  def new
+    @training_plan = TrainingPlan.new
+  end
 
-    def create
+  def create
+    @training_plan = TrainingPlan.new(training_plan_params)
+    @training_plan.exercise = 
+    @training_plan.training =
+    if @training_plan.save
+      redirect_to root_path
+    else
+      render :new
     end
-    
+  end
+
+  private
+
+  def training_plan_params
+    params.require(:training_plan).permit(:exercise, :training)
+  end
+  
 end
