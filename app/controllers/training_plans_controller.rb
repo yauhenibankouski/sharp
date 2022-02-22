9a8205fb87dd45e5eed4b3e3c914ae384667202d
bassem riachy <bassem.riachy@gmail.com>
2022-02-22 21:31:51 +0100

    Added login link to home page + generated route and controller for training_plans

---
+++ app/controllers/training_plans_controller.rb
@@ -0,0 +1,9 @@
+class TrainingPlansController < ApplicationController
+
+    def new
+    end
+
+    def create
+    end
+    
+end
---
+++ test/controllers/training_plans_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class TrainingPlansControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
