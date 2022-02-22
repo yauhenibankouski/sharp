ce72e5eb6e3c62fd9613907cc1d39b19ab6073cc
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-02-22 20:23:55 +0100

    added new and crete methods for creating a new exercise

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,7 +1,21 @@
 class ExercisesController < ApplicationController
   def new
+    @exercise = Exercise.new
   end
 
   def create
+    @exercise = Exercise.new(exercise_params)
+    @exercise.user = current_user
+    if @exercise.save
+      redirect_to root_path # Need to update where to re-direct after exercise creation
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def exercise_params
+    params.require(:exercise).permit(:title, :description, :technique, :sets, :repetitions)
   end
 end
