d249da2b40d94704bb81a669d5660e2d189229a5
bassem riachy <bassem.riachy@gmail.com>
2022-02-24 20:40:27 +0100

    Created all the views for training plan

--- app/controllers/training_plans_controller.rb
+++ app/controllers/training_plans_controller.rb
@@ -1,6 +1,8 @@
 class TrainingPlansController < ApplicationController
+  before_action :set_training_plan, only: %i[show edit update destroy ]
 
   def index
+    @training_plans = TrainingPlan.all
   end
 
   def show
@@ -19,10 +21,30 @@ class TrainingPlansController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @training_plan.update(training_plan_params)
+      redirect_to @training_plan, notice: 'Training plan was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @training_plan.destroy
+    redirect_to training_plans_path, notice: 'Training plan was successfully deleted.'
+  end
+
   private
 
   def training_plan_params
     params.require(:training_plan).permit(:exercise, :training)
   end
   
+  def set_training_plan
+    @training_plan = TrainingPlan.find(params[:id])
+  end
+
 end
