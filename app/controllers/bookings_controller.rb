a3645b26426ac10d0da3ec4bb8fb3e4a7fded5ec
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-02-26 22:25:36 +0100

    added booking create form in find an expert view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,16 @@
 class BookingsController < ApplicationController
   def index
     @available_trainers = User.includes(:bookings).where(trainer: true).where.not(id: current_user.id)
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to bookings_path, notice: "Request sent to #{@booking.user.first_name.capitalize}!"
+    else
+      render :index
+    end
   end
 
   def clients
@@ -11,5 +21,11 @@ class BookingsController < ApplicationController
   def trainers
     bookings = Booking.where(client: current_user)
     @trainers = bookings.map(&:user)
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:user_id, :client_id, :status)
   end
 end
