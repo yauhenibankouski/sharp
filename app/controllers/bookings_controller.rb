58e1598996180f3a1a48aae5ab12d8e107814822
Whalton Hippertt <whalton.hippertt@hotmail.com>
2022-02-27 14:44:13 +0100

    clients and trainers pages displaying well. Bookings/index routes to be defined

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,15 @@
 class BookingsController < ApplicationController
   def index
+
   end
 
   def clients
+    @bookings = Booking.where(client: current_user)
     @clients = current_user.bookings.map(&:client)
   end
 
   def trainers
-    bookings = Booking.where(client: current_user)
-    @trainers = bookings.map(&:user)
+    @trainers = current_user.bookings.map(&:user)
   end
+
 end
