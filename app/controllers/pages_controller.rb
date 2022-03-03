e3ba7a3c166b1f0c66ac457ce3867d7a00157169
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-03-03 20:41:48 +0100

    updated home page links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @clients = current_user.bookings.map(&:client)
+    @trainers = Booking.where(client: current_user).map(&:user)
+    # raise
   end
 end
