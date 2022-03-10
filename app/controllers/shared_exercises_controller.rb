2bd9b327310bc21c70ac0fb8374365947e61824a
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-03-10 15:58:49 +0100

    added edit button and edit feature for shared exercise by the trainer

--- app/controllers/shared_exercises_controller.rb
+++ app/controllers/shared_exercises_controller.rb
@@ -7,6 +7,23 @@ class SharedExercisesController < ApplicationController
     @message = Message.new
   end
 
+  def edit
+    @booking = Booking.find(params[:booking_id])
+    @shared_training_plan = SharedTrainingPlan.find(params[:shared_training_plan_id])
+    @shared_exercise = SharedExercise.find(params[:id])
+  end
+
+  def update
+    @shared_exercise = SharedExercise.find(params[:id])
+    @booking = Booking.find(params[:booking_id])
+    @shared_training_plan = SharedTrainingPlan.find(params[:shared_training_plan_id])
+    if @shared_exercise.update(shared_exercise_params)
+      redirect_to booking_shared_training_plan_shared_exercise_path(@booking, @shared_training_plan, @shared_exercise)
+    else
+      render :edit
+    end
+  end
+
   def destroy
     @booking = Booking.find(params[:booking_id])
     @shared_exercise = SharedExercise.find(params[:id])
@@ -14,5 +31,11 @@ class SharedExercisesController < ApplicationController
     @shared_training_plan.destroy
     stp = SharedTrainingPlan.find_by(booking: @booking)
     redirect_to booking_shared_training_plan_path(@booking, stp)
+  end
+
+  private
+
+  def shared_exercise_params
+    params.require(:shared_exercise).permit(:description, :technique, :sets, :repetitions, :photo)
   end
 end
