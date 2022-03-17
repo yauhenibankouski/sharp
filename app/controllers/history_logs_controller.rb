4d2a16a2e93d3b9597ffc2b94cec24cb27b99a31
Yauheni Bankouski <yauheni.bankouski@gmail.com>
2022-03-17 17:50:05 +0100

    fixed layouts

--- app/controllers/history_logs_controller.rb
+++ app/controllers/history_logs_controller.rb
@@ -4,7 +4,7 @@ class HistoryLogsController < ApplicationController
     @booking = Booking.find(params[:booking_id])
     @shared_exercise = SharedExercise.find(params[:shared_exercise_id])
     @shared_training_plan = SharedTrainingPlan.find(params[:shared_training_plan_id])
-    @history_log = HistoryLog.where(shared_exercise: @shared_exercise)
+    @history_log = HistoryLog.where(shared_exercise: @shared_exercise).order("created_at DESC")
   end
 
   def new
